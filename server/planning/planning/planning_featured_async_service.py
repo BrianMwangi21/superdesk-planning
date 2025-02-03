dc8f2a2f8f9d78d5bde91560fef156f77c25958b
Brian Mwangi <mwangikabiru21@gmail.com>
2025-02-03 11:28:29 +0300

    Suggested fixes

--- server/planning/planning/planning_featured_async_service.py
+++ server/planning/planning/planning_featured_async_service.py
@@ -2,13 +2,12 @@ from datetime import datetime
 from typing import Any
 from copy import deepcopy
 
-from apps.archive.common import update_dates_for
 from apps.auth import get_user_id
+from bson import ObjectId
 from planning.core.service import BasePlanningAsyncService
 from planning.common import (
     get_version_item_for_post,
     enqueue_planning_item,
-    set_original_creator,
 )
 from planning.planning.planning_service import PlanningAsyncService
 from planning.types import PlanningFeaturedResourceModel
@@ -25,9 +24,10 @@ class PlanningFeaturedAsyncService(BasePlanningAsyncService[PlanningFeaturedReso
     """Async Service class for the planning featured model."""
 
     async def on_create(self, docs: list[PlanningFeaturedResourceModel]):
+        await super().on_create(docs)
+
         for doc in docs:
-            doc_dict = doc.to_dict()
-            date = utc_to_local(doc_dict.get("tz") or get_app_config("DEFAULT_TIMEZONE"), doc_dict.get("date"))
+            date = utc_to_local(doc.tz or get_app_config("DEFAULT_TIMEZONE"), doc.date)
             _id = date.strftime(ID_DATE_FORMAT)
 
             search_request = SearchRequest(where={"_id": _id})
@@ -35,39 +35,32 @@ class PlanningFeaturedAsyncService(BasePlanningAsyncService[PlanningFeaturedReso
             if await items.count() > 0:
                 raise SuperdeskApiError.badRequestError(message="Featured story already exists for this date.")
 
-            await self.validate_featured_attrribute(doc_dict.get("items", []))
-            doc_dict["_id"] = _id
-            await self.post_featured_planning(doc_dict)
-            # set the author
-            set_original_creator(doc_dict)
-
-            # set timestamps
-            update_dates_for(doc_dict)
-            doc = PlanningFeaturedResourceModel(**doc_dict)
+            await self.validate_featured_attrribute(doc.items)
+            doc.id = _id
+            await self.post_featured_planning(doc)
 
     async def on_created(self, docs: list[PlanningFeaturedResourceModel]):
         for doc in docs:
             await self.enqueue_published_item(doc.to_dict(), doc.to_dict())
 
     async def on_update(self, updates: dict[str, Any], original: PlanningFeaturedResourceModel):
+        await super().on_update(updates, original)
         # Find all planning items in the list
         added_featured = [item_id for item_id in updates.get("items") or [] if item_id not in original.items or []]
         await self.validate_featured_attrribute(added_featured)
-        updates["version_creator"] = str(get_user_id())
-        await self.post_featured_planning(updates, original.to_dict())
+        await self.post_featured_planning(PlanningFeaturedResourceModel(**updates), original)
 
     async def on_updated(self, updates: dict[str, Any], original: PlanningFeaturedResourceModel):
         await self.enqueue_published_item(updates, original.to_dict())
 
-    async def post_featured_planning(self, updates: dict[str, Any], original: dict[str, Any] | None = None):
-        if original is None:
-            original = {}
-
-        if updates.get("posted", False):
-            await self.validate_post_status(updates.get("items", original.get("items", [])))
-            updates["posted"] = True
-            updates["last_posted_time"] = utcnow()
-            updates["last_posted_by"] = str(get_user_id())
+    async def post_featured_planning(
+        self, updates: PlanningFeaturedResourceModel, original: PlanningFeaturedResourceModel | None = None
+    ):
+        if updates.posted:
+            await self.validate_post_status(updates.items if updates.items else (original.items if original else []))
+            updates.posted = True
+            updates.last_posted_time = utcnow()
+            updates.last_posted_by = ObjectId(get_user_id())
 
     async def enqueue_published_item(self, updates: dict[str, Any], original: dict[str, Any]):
         if updates.get("posted", False):
--- server/planning/types/planning_featured.py
+++ server/planning/types/planning_featured.py
@@ -11,10 +11,10 @@ from superdesk.core.resources.validators import validate_data_relation_async
 class PlanningFeaturedResourceModel(BasePlanningModel):
     date: datetime = Field(default_factory=utcnow)
     items: list = Field(default_factory=list)
-    tz: str
-    posted: bool
-    last_posted_time: datetime = Field(default_factory=utcnow)
-    last_posted_by: Annotated[fields.ObjectId, validate_data_relation_async("users")]
+    tz: str | None = None
+    posted: bool = False
+    last_posted_time: datetime | None = None
+    last_posted_by: Annotated[fields.ObjectId | None, validate_data_relation_async("users")] = None
     firstcreated: datetime = Field(default_factory=utcnow)
     versioncreated: datetime = Field(default_factory=utcnow)
     item_type: str = Field(
